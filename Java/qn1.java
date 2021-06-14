654cb0948ef0b5b91b6dab3d7fa45da084a6025a
ShipraPatwal <85698677+ShipraPatwal@users.noreply.github.com>
2021-06-14 16:40:29 +0530

    Add files via upload

---
+++ Java/qn1.java
@@ -0,0 +1,12 @@
+// java program to demonstrate to UpperCase() method.
+
+class DemoString
+{
+  public static void main(String[] args)
+  {
+  String str1 = new String("converting upper case");
+   System.out.println(str1);
+    str1 = str1.toUpperCase();
+   System.out.println(str1);
+}
+}
